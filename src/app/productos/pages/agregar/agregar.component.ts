44758528da5aef456b8ee9ce26c6fa3ee74d5288
Alejandro González Reyes <alexglzrys@gmail.com>
2021-12-22 17:00:58 -0600

    reaccionar a cambios de inputs en tiempo real

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,6 +9,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class AgregarComponent implements OnInit {
 
+  mensaje: string = 'Este es un campo obligatorio';
+  color: string = 'teal';
+
   // Inicializar el formulario reactivo
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', Validators.required]
@@ -21,6 +24,14 @@ export class AgregarComponent implements OnInit {
 
   tieneError(campo: string): boolean {
     return this.miFormulario.get(campo)?.invalid || false;
+  }
+
+  cambiarMensaje(): void {
+    this.mensaje = 'El nombre es un dato requerido';
+  }
+
+  cambiarColor(): void {
+    this.color = 'gold';
   }
 
 }
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Directive({
   // El selector que usaremos para invocar esta directiva
   selector: '[error-msg]'
 })
-export class ErrorMsgDirective implements OnInit {
+export class ErrorMsgDirective implements OnInit, OnChanges {
 
   // Las directivas tambien pueden recibir entrada de información
   // Una buena idea es mantener un valor por defecto si el usuario no pasa esta información
@@ -27,24 +27,42 @@ export class ErrorMsgDirective implements OnInit {
     this.elementHTMLRef = el;
   }
 
+  // Detectar cambios de valor en los Inputs de la directiva
+  ngOnChanges(changes: SimpleChanges): void {
+    // Se hace de esta forma ya que para establecer un color o mensaje, los métodos correspondientes a estas tareas,
+    // solo se ejecutan una vez dentro del ngOnInit declarado en esta directiva
+    console.log(changes)
+
+    // Hay que condicionar por cada Input() declarado en la directiva
+    if (changes.mensaje) {
+      this.elementHTMLRef.nativeElement.innerHTML = changes.mensaje.currentValue;
+    }
+
+    if (changes.color) {
+      this.elementHTMLRef.nativeElement.style.color = changes.color.currentValue;
+    }
+  }
+
   ngOnInit(): void {
       console.log('ngOnInit - Directiva')
       this.setClass()
+
+      // el color y mensaje no cambian de forma dinámica en el futuro, dado que solo se establecen al inicializar la directiva
       this.setColor()
       this.setMensaje()
   }
 
-  setColor() {
+  setColor(): void {
     // Cambiar el color del texto del elemento HTML afectado por esta directiva
     this.elementHTMLRef.nativeElement.style.color = this.color;
   }
 
-  setMensaje() {
+  setMensaje(): void {
     // Cambiar el texto interno del elemeno HTML afectado por esta directiva
     this.elementHTMLRef.nativeElement.innerHTML = this.mensaje;
   }
 
-  setClass() {
+  setClass(): void {
     this.elementHTMLRef.nativeElement.classList.add('form-text');
   }
 
